e36ff61dea64c17035bbb90eb39e5819d83c734a
letanphucoro <letanphuc@vn.oro.com>
2016-08-29 17:49:11 +0700

    CRUD a item article

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
+  get 'articles/index'
+  get 'articles/show'
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
